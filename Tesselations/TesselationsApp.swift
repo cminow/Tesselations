c1e1fe39cb8a2618d1f5b94fac3186354eda8933
Charlie Minow <minow@mac.com>
2025-05-22 11:33:01 -0600

    Initial Commit

---
+++ Tesselations/TesselationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesselationsApp.swift
+//  Tesselations
+//
+//  Created by Charlie Minow on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TesselationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TesselationsTests/TesselationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TesselationsTests.swift
+//  TesselationsTests
+//
+//  Created by Charlie Minow on 5/22/25.
+//
+
+import Testing
+@testable import Tesselations
+
+struct TesselationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
