7133cfb3401ae045d2cea331aa63d9c1806bf45a
Charlie Minow <minow@mac.com>
2025-05-22 20:01:36 -0600

    Approach the triangle differently, so I can inset them a bit and hide the anti-aliasing on the edges.

--- Tesselations/Destination Views/TrianglesView.swift
+++ Tesselations/Destination Views/TrianglesView.swift
@@ -5,8 +5,6 @@
 //  Created by Charlie Minow on 5/22/25.
 //
 
-// This is based on code from hexagons, since they're closely related.
-
 import SwiftUI
 
 struct TrianglesView: View {
@@ -16,44 +14,53 @@ struct TrianglesView: View {
         VStack {
             Canvas { context, size in
                 let rows = size.height / hexRadius
-                let columns = (size.width * 1.5) / hexRadius
+                let columns = (size.width * 1.86667) / hexRadius
+                let insetFactor: CGFloat = 0.95
                 
                 for row in 0...Int(rows) {
                     for column in 0...Int(columns) {
-                        var columnOffset: CGFloat = 0.0
+                        var rowOffset: CGFloat = 0.0
+                        if row % 2 == 1 {
+                            rowOffset -= hexRadius * 0.8666667
+                        }
+                        // offset is sqrt(3) for the horizonal, but the constant is close enough.
+                        let center = CGPoint(x: Double(column) * 1.732051 * hexRadius + rowOffset, y: Double(row) * hexRadius * 1.5)
+                        let mainPath: Path = trianglePath(hexCenter: center,
+                                                          radius: hexRadius * insetFactor)
+                        let brightness: Double = .random(in: 0.50...0.75)
 
-                        if column % 2 != 0 {
-                            columnOffset = hexRadius * sqrt(3) / 2.0
-                        }
-                        let center = CGPoint(x: Double(column) * hexRadius * 1.5, y: Double(row) * hexRadius * sqrt(3) + columnOffset)
-                        for index in 0...5 {
-                            let mainPath: Path = trianglePath(hexCenter: center, radius: hexRadius, cornerIndex: index)
-                            let brightness: Double = .random(in: 0.50...0.750)
-                            context.fill(mainPath, with: .color(.init(hue: 0.0, saturation: 0.0, brightness: brightness)))
-                        }
+                        context.fill(mainPath, with: .color(.init(hue: 0.0,
+                                                                  saturation: 0.0,
+                                                                  brightness: brightness,
+                                                                  opacity: 1.0)))
+
+                        let secondaryBrightness: Double = .random(in: 0.50...0.75)
+                        let secondaryPath: Path = trianglePath(hexCenter: CGPoint(x: center.x + (hexRadius * 0.86667),
+                                                                                  y: center.y + (hexRadius * 0.5)),
+                                                               radius: -hexRadius * insetFactor)
+                        context.fill(secondaryPath, with: .color(.init(hue: 0.0,
+                                                                       saturation: 0.0,
+                                                                       brightness: secondaryBrightness,
+                                                                       opacity: 1.0)))
                     }
                 }
             }
         }
     }
 
-    func trianglePath(hexCenter: CGPoint, radius: CGFloat, cornerIndex: Int) -> Path {
+    func trianglePath(hexCenter: CGPoint, radius: CGFloat) -> Path {
         var path = Path()
-        path.move(to: hexagonCorner(center: hexCenter, radius: radius, cornerIndex: cornerIndex))
-        path.addLine(to: hexagonCorner(center: hexCenter, radius: radius, cornerIndex: cornerIndex + 1))
-        path.addLine(to: hexCenter)
+        let topPoint: CGPoint = CGPoint(x: hexCenter.x, y: hexCenter.y + radius)
+        let rightPoint: CGPoint = CGPoint(x: hexCenter.x + (radius * 0.86667),
+                                          y: hexCenter.y - (radius * 0.5))
+        let leftPoint: CGPoint = CGPoint(x: hexCenter.x - (radius * 0.86667),
+                                         y: hexCenter.y - (radius * 0.5))
+        path.move(to: topPoint)
+        path.addLine(to: rightPoint)
+        path.addLine(to: leftPoint)
+        path.addLine(to: topPoint)
         path.closeSubpath()
         return path
-    }
-    
-    func hexagonCorner(center: CGPoint, radius: CGFloat, cornerIndex: Int) -> CGPoint {
-        let angleDegrees: CGFloat = (60.0 * CGFloat(cornerIndex))// - 30.0
-        let angleRadians: CGFloat = .pi / 180.0 * angleDegrees
-        let corner = CGPoint(
-            x: center.x + radius * cos(angleRadians),
-            y: center.y + radius * sin(angleRadians)
-        )
-        return corner
     }
 }
 
