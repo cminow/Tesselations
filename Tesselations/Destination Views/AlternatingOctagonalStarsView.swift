ba8c3fdc0c680b29d8b712f4a8b293f700b13085
Charlie Minow <minow@mac.com>
2025-06-07 14:29:23 -0600

    Add the flat diamon path that goes between the tiles.

--- Tesselations/Destination Views/AlternatingOctagonalStarsView.swift
+++ Tesselations/Destination Views/AlternatingOctagonalStarsView.swift
@@ -19,7 +19,12 @@ struct AlternatingOctagonalStarsView: View {
                 for row in 0...Int(rows) {
                     remainder = row % 2
                     for column in 0...Int(columns) {
+                        let center: CGPoint = CGPoint(x: Double(column) * radius * 2.0, y: Double(row) * radius * 2.0)
                         
+                        let mainLayoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius * sqrt(2), inscribedPolygonInitialAngle: .degrees(45))
+//                        context.stroke(mainLayoutCircle.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+//                        context.stroke(mainLayoutCircle.inscribedSquare.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+
                         var backColor: GraphicsContext.Shading = .color(.white)
                         var middleColor: GraphicsContext.Shading = .color(.blue)
                         var frontColor: GraphicsContext.Shading = .color(.yellow)
@@ -28,11 +33,21 @@ struct AlternatingOctagonalStarsView: View {
                             backColor =  .color(.blue)
                             middleColor = .color(.yellow)
                             frontColor = .color(.blue)
+                            
+                            for (index, point) in mainLayoutCircle.inscribedSquare.points.enumerated() {
+                                let flatDiamondPath: Path = flatDiamondPath(center: point, radius: radius * 0.4, angleOffset: .degrees(0.0 * Double(index)), corner: index)
+                                context.fill(flatDiamondPath, with: .color(.blue))
+                            }
+                            
+                        } else {
+                            let backOctagon: Octagon =  Octagon(center: center, radius: radius * 1.1, initialAngle: .degrees(22.5))
+                            context.fill(backOctagon.path, with: .color(.blue))
                         }
 
-                        let center: CGPoint = CGPoint(x: Double(column) * radius * 2.0, y: Double(row) * radius * 2.0)
+                        
                         let backStar: Octagon = Octagon(center: center, radius: radius, initialAngle: .degrees(0.0))
                         context.fill(backStar.stellatedPath, with: backColor)
+                        
                         let middleLayoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius * 0.75, inscribedPolygonInitialAngle: .degrees(22.50))
                         context.fill(middleLayoutCircle.inscribedOctagon.secondStellatedPath, with: middleColor)
                         let frontLayoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius * 0.45)
@@ -42,8 +57,40 @@ struct AlternatingOctagonalStarsView: View {
             }
         }
     }
+
+    private func flatDiamondPath(center: CGPoint, radius: CGFloat, angleOffset: Angle, corner: Int) -> Path {
+        var rotationAngle: Angle
+        if corner % 2 == 1 {
+            rotationAngle = .degrees(45)
+        } else {
+            rotationAngle = .degrees(135)
+        }
+        var path = Path()
+        var actualRadius: CGFloat = radius
+        path.move(to: cornerPoint(center: center, radius: actualRadius, cornerIndex: 0, initialAngle: rotationAngle + angleOffset))
+        for index in 1...3 {
+            if index % 2 == 1 {
+                actualRadius = radius * (sqrt(2) / 3)
+            } else {
+                actualRadius = radius
+            }
+            path.addLine(to: cornerPoint(center: center, radius: actualRadius, cornerIndex: index, initialAngle: rotationAngle + angleOffset))
+        }
+        path.addLine(to: cornerPoint(center: center, radius: radius, cornerIndex: 0, initialAngle: rotationAngle + angleOffset))
+        path.closeSubpath()
+        return path
+    }
+
+    private func cornerPoint(center: CGPoint, radius: CGFloat, cornerIndex: Int, initialAngle: Angle) -> CGPoint {
+        let angle: CGFloat = (CGFloat(90).radians * CGFloat(cornerIndex)) + initialAngle.radians
+        let corner = CGPoint(
+            x: center.x + radius * cos(angle),
+            y: center.y + radius * sin(angle)
+        )
+        return corner
+    }
 }
 
 #Preview {
-    AlternatingOctagonalStarsView(radius: 64)
+    AlternatingOctagonalStarsView(radius: 32)
 }
