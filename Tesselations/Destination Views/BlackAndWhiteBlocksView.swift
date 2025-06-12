b26095164ffb7a8c5bede94b308cec96ab9c325f
Charlie Minow <minow@mac.com>
2025-06-12 12:58:13 -0600

    Factor the path layout code into its own function.

--- Tesselations/Destination Views/BlackAndWhiteBlocksView.swift
+++ Tesselations/Destination Views/BlackAndWhiteBlocksView.swift
@@ -25,65 +25,19 @@ struct BlackAndWhiteBlocksView: View {
                             x: Double(column) * blockWidth * 2.0,
                             y: Double(row) * blockWidth * 2.0
                         )
-                        let layoutCircle1: LayoutCircle = LayoutCircle(center: center, radius: blockWidth, inscribedPolygonInitialAngle: .degrees(45.0))
 
                         let shoulderIndent: CGFloat = blockWidth / 6.0
-                        let halfBlockWidth: CGFloat = blockWidth / 2.0
-                        
+
                         for _ in 0...1 {
-                            var path: Path = Path()
-                            path.move(to: center)
-                            path.addLine(to: CGPoint(x: center.x + halfBlockWidth - shoulderIndent, y: center.y - halfBlockWidth))
-                            path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y - halfBlockWidth))
-                            path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y - halfBlockWidth + shoulderIndent))
-                            path.addLine(to: CGPoint(x: center.x + blockWidth, y: center.y))
-                            path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth -  shoulderIndent))
-                            path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth))
-                            path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth))
-                            path.addLine(to: CGPoint(x: center.x + halfBlockWidth - shoulderIndent, y: center.y + halfBlockWidth))
-                            path.addLine(to: center)
-                            path.closeSubpath()
-                            context.fill(path, with: .color(.black))
+                            let paths = blockPaths(center: center, blockWidth: blockWidth, shoulderIndent: shoulderIndent)
                             
-                            var path2: Path = Path()
-                            path2.move(to: center)
-                            path2.addLine(to: CGPoint(x: center.x - halfBlockWidth + shoulderIndent, y: center.y - halfBlockWidth))
-                            path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y - halfBlockWidth))
-                            path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y - halfBlockWidth + shoulderIndent))
-                            path2.addLine(to: CGPoint(x: center.x - blockWidth, y: center.y))
-                            path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth -  shoulderIndent))
-                            path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth))
-                            path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth))
-                            path2.addLine(to: CGPoint(x: center.x - halfBlockWidth + shoulderIndent, y: center.y + halfBlockWidth))
-                            path2.addLine(to: center)
-                            path2.closeSubpath()
-                            context.fill(path2, with: .color(.black))
-                            
-                            var path3: Path = Path()
-                            path3.move(to: center)
-                            path3.addLine(to: CGPoint(x: center.x - halfBlockWidth + shoulderIndent, y: center.y - halfBlockWidth))
-                            path3.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y - halfBlockWidth))
-                            path3.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y - halfBlockWidth - shoulderIndent))
-                            path3.addLine(to: CGPoint(x: center.x, y: center.y - blockWidth))
-                            path3.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y - halfBlockWidth - shoulderIndent))
-                            path3.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y - halfBlockWidth))
-                            path3.addLine(to: CGPoint(x: center.x + halfBlockWidth -  shoulderIndent, y: center.y - halfBlockWidth))
-                            path3.addLine(to: center)
-                            path3.closeSubpath()
-                            context.fill(path3, with: .color(.blue))
-                            
-                            var path4: Path = Path()
-                            path4.move(to: center)
-                            path4.addLine(to: CGPoint(x: center.x - halfBlockWidth + shoulderIndent, y: center.y + halfBlockWidth))
-                            path4.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth))
-                            path4.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth + shoulderIndent))
-                            path4.addLine(to: CGPoint(x: center.x, y: center.y + blockWidth))
-                            path4.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth + shoulderIndent))
-                            path4.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth))
-                            path4.addLine(to: CGPoint(x: center.x + halfBlockWidth - shoulderIndent, y: center.y + halfBlockWidth))
-                            path4.addLine(to: center)
-                            path4.closeSubpath()
-                            context.fill(path4, with: .color(.blue))
+                            for (index, path) in paths.enumerated() {
+                                if index < 2 {
+                                    context.fill(path, with: .color(.black))
+                                } else {
+                                    context.fill(path, with: .color(.blue))
+                                }
+                            }
                             
                             center.x += blockWidth
                             center.y += blockWidth
@@ -92,6 +46,68 @@ struct BlackAndWhiteBlocksView: View {
                 }
             }
         }
+    }
+
+    private func blockPaths(center: CGPoint, blockWidth: CGFloat, shoulderIndent: CGFloat) -> [Path] {
+        var paths: [Path] = []
+        
+        let halfBlockWidth: CGFloat = blockWidth / 2.0
+        
+        var path: Path = Path()
+        path.move(to: center)
+        path.addLine(to: CGPoint(x: center.x + halfBlockWidth - shoulderIndent, y: center.y - halfBlockWidth))
+        path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y - halfBlockWidth))
+        path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y - halfBlockWidth + shoulderIndent))
+        path.addLine(to: CGPoint(x: center.x + blockWidth, y: center.y))
+        path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth -  shoulderIndent))
+        path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth))
+        path.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth))
+        path.addLine(to: CGPoint(x: center.x + halfBlockWidth - shoulderIndent, y: center.y + halfBlockWidth))
+        path.addLine(to: center)
+        path.closeSubpath()
+        paths.append(path)
+        
+        var path2: Path = Path()
+        path2.move(to: center)
+        path2.addLine(to: CGPoint(x: center.x - halfBlockWidth + shoulderIndent, y: center.y - halfBlockWidth))
+        path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y - halfBlockWidth))
+        path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y - halfBlockWidth + shoulderIndent))
+        path2.addLine(to: CGPoint(x: center.x - blockWidth, y: center.y))
+        path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth -  shoulderIndent))
+        path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth))
+        path2.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth))
+        path2.addLine(to: CGPoint(x: center.x - halfBlockWidth + shoulderIndent, y: center.y + halfBlockWidth))
+        path2.addLine(to: center)
+        path2.closeSubpath()
+        paths.append(path2)
+        
+        var path3: Path = Path()
+        path3.move(to: center)
+        path3.addLine(to: CGPoint(x: center.x - halfBlockWidth + shoulderIndent, y: center.y - halfBlockWidth))
+        path3.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y - halfBlockWidth))
+        path3.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y - halfBlockWidth - shoulderIndent))
+        path3.addLine(to: CGPoint(x: center.x, y: center.y - blockWidth))
+        path3.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y - halfBlockWidth - shoulderIndent))
+        path3.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y - halfBlockWidth))
+        path3.addLine(to: CGPoint(x: center.x + halfBlockWidth -  shoulderIndent, y: center.y - halfBlockWidth))
+        path3.addLine(to: center)
+        path3.closeSubpath()
+        paths.append(path3)
+        
+        var path4: Path = Path()
+        path4.move(to: center)
+        path4.addLine(to: CGPoint(x: center.x - halfBlockWidth + shoulderIndent, y: center.y + halfBlockWidth))
+        path4.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth))
+        path4.addLine(to: CGPoint(x: center.x - halfBlockWidth, y: center.y + halfBlockWidth + shoulderIndent))
+        path4.addLine(to: CGPoint(x: center.x, y: center.y + blockWidth))
+        path4.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth + shoulderIndent))
+        path4.addLine(to: CGPoint(x: center.x + halfBlockWidth, y: center.y + halfBlockWidth))
+        path4.addLine(to: CGPoint(x: center.x + halfBlockWidth - shoulderIndent, y: center.y + halfBlockWidth))
+        path4.addLine(to: center)
+        path4.closeSubpath()
+        paths.append(path4)
+        
+        return paths
     }
 }
 
