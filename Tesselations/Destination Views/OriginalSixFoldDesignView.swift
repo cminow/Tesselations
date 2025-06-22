366bf04e131a37186918526b27bbb45ba0bc484e
Charlie Minow <minow@mac.com>
2025-06-22 11:22:58 -0600

    Mess with the colors some more.

--- Tesselations/Destination Views/OriginalSixFoldDesignView.swift
+++ Tesselations/Destination Views/OriginalSixFoldDesignView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct OriginalSixFoldDesignView: View {
     var radius: CGFloat
+    let cornerColors: [GraphicsContext.Shading] = [
+        .color(.init(hue: 0.50, saturation: 1.0, brightness: 0.350)),
+        .color(.init(hue: 0.50, saturation: 1.0, brightness: 0.450)),
+        .color(.init(hue: 0.50, saturation: 1.0, brightness: 0.5))
+    ]
+
     var body: some View {
         VStack {
             Canvas { context, size in
                 let rows: CGFloat = size.height / radius
                 let columns: CGFloat = size.width / radius
+
+                let lineWidth: CGFloat = radius * 0.025
+
+                let rect: CGRect = context.clipBoundingRect
+                context.fill(Rectangle().path(in: rect), with: .color(.init(hue: 0.0, saturation: 0.5, brightness: 0.25)))
                 
                 for row in 0...Int(rows) {
                     for column in 0...Int(columns) {
@@ -25,37 +36,94 @@ struct OriginalSixFoldDesignView: View {
                         let center = CGPoint(x: Double(column) * radius * 1.5, y: Double(row) * radius * .sqrt3 + columnOffset)
                         
                         let layoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius)
-                        context.stroke(layoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+//                        context.stroke(layoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+
+                        let secondaryLayoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius * 0.86667, inscribedPathDirection: .northSouth)
+//                        context.stroke(secondaryLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
 
                         for (index, point) in layoutCircle.inscribedHexagon.points.enumerated() {
                             let hexagon = cornerHexagon(center: point, radius: radius / 3.0 / .sqrt3, cornerIndex: index)
-                            context.stroke(hexagon, with: .color(.black), style: .init(lineWidth: 1.0, lineCap: .round, lineJoin: .round))
+                            context.fill(hexagon, with: cornerColors[index % 3])
+                            context.stroke(hexagon, with: .color(.white), style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
                         }
 
                         let outerLayoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius * 0.81)
-//                        context.stroke(outerLayoutCircle.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
-                        context.stroke(outerLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+//                        context.stroke(outerLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+//                        context.stroke(outerLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
                         
                         let innerLayoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius / .sqrt3 / 2.0, inscribedPathDirection: .northSouth)
-                        context.stroke(innerLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+//                        context.stroke(innerLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
                         let middleLayoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius / 2.0)
-                        context.stroke(middleLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
-                        context.stroke(middleLayoutCircle.inscribedHexagon.inscribedSixPointStarPath, with: .color(.cyan), style: .init(lineWidth: 1.0))
+//                        context.stroke(middleLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+//                        context.stroke(middleLayoutCircle.inscribedHexagon.inscribedSixPointStarPath, with: .color(.cyan), style: .init(lineWidth: 1.0))
                         let thirdLayoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius / .sqrt3, inscribedPathDirection: .northSouth)
-                        context.stroke(thirdLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+//                        context.stroke(thirdLayoutCircle.path, with: .color(.cyan), style: .init(lineWidth: 40.0))
                         
                         for index in 0...5 {
+                            var brightness: Double = 0.350
+                            if index % 2 == 0 {
+                                brightness = 0.5
+                            }
                             let petalPath: Path = outerPetalPath(innerLayoutCircle: thirdLayoutCircle, outerLayoutCircle: outerLayoutCircle, radius: radius, cornerIndex: index)
-//                            context.fill(petalPath, with: .color(.blue))
-                            context.stroke(petalPath, with: .color(.black), style: .init(lineWidth: 1.0))
+                            context.fill(petalPath, with: .color(.init(hue: 0.025, saturation: 0.750, brightness: brightness)))
+                            context.stroke(petalPath, with: .color(.white), style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
                         }
                         
                         let kiteLayoutCircle: LayoutCircle = LayoutCircle(center: center, radius: radius / 4.0)
-                        context.stroke(kiteLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
+//                        context.stroke(kiteLayoutCircle.inscribedHexagon.path, with: .color(.cyan), style: .init(lineWidth: 1.0))
                         
                         for index in 0...5 {
+                            var brightness: Double = 0.90
+                            if index % 2 == 0 {
+                                brightness = 0.8
+                            }
                             let kitePath: Path = kiteLayoutPath(layoutCircle1: kiteLayoutCircle, layoutCircle2: innerLayoutCircle, cornerIndex: index)
-                            context.stroke(kitePath, with: .color(.black), style: .init(lineWidth: 1.0))
+                            let fillColor: GraphicsContext.Shading = .color(.init(hue: 1.0, saturation: 1.0, brightness: brightness))
+                            context.fill(kitePath, with: fillColor)
+                            context.stroke(kitePath, with: .color(.white), style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+                        }
+
+                        for index in 0...5 {
+                            let kitePath: Path = outerKitePath(innerLayoutCircle: middleLayoutCircle, outerLayoutCircle: outerLayoutCircle, radius: radius, cornerIndex: index)
+                            context.fill(kitePath, with: .color(.init(red: 0.350, green: 0.0, blue: 0.0)))
+                            context.stroke(kitePath, with: .color(.white), style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+                        }
+
+                        for index in 0...5 {
+                            var brightness: Double = 0.50
+                            if index % 2 == 0 {
+                                brightness = 0.65
+                            }
+                            let diamondPath = diamondPath(innerLayoutCircle: innerLayoutCircle,
+                                                          middleLayoutCircle: middleLayoutCircle,
+                                                          outerLayoutCircle: thirdLayoutCircle,
+                                                          cornerIndex: index)
+                            context.fill(diamondPath, with: .color(.init(hue: 0.0, saturation: 1.0, brightness: brightness)))
+                            context.stroke(diamondPath, with: .color(.white), style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+                        }
+
+                        for index in 0...5 {
+                            var brightness: Double = 0.5
+                            if index % 2 == 0 {
+                                brightness = 0.7
+                            }
+                            let starPoint: Path = startPointPath(innerLayoutCircle: innerLayoutCircle, outerLayoutCircle: middleLayoutCircle, cornerIndex: index)
+                            context.fill(starPoint, with: .color(.init(hue: 0.075, saturation: 1.0, brightness: brightness)))
+                            context.stroke(starPoint, with: .color(.white), style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+                        }
+
+                        for index in 0...5 {
+                            var brightness: Double = 0.35
+                            if index % 2 == 0 {
+                                brightness = 0.5
+                            }
+                            let outerDiamond: Path = edgeFiller(innerLayoutCircle: thirdLayoutCircle,
+                                                                middleLayoutCircle: outerLayoutCircle,
+                                                                outerLayoutCircle: secondaryLayoutCircle,
+                                                                radius: radius,
+                                                                cornerIndex: index)
+                            context.fill(outerDiamond, with: .color(.init(hue: 0.5, saturation: 1.0, brightness: brightness)))
+                            context.stroke(outerDiamond, with: .color(.white), style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
                         }
                     }
                 }
@@ -63,6 +131,58 @@ struct OriginalSixFoldDesignView: View {
         }
     }
 
+    private func edgeFiller(innerLayoutCircle: LayoutCircle,
+                            middleLayoutCircle: LayoutCircle,
+                            outerLayoutCircle: LayoutCircle,
+                            radius: CGFloat,
+                            cornerIndex: Int) -> Path {
+        let cornerHexagon: Hexagon = Hexagon(center: middleLayoutCircle.inscribedHexagon.points[cornerIndex], radius: radius / 3.0 / .sqrt3)
+        let cornerHexagon2: Hexagon = Hexagon(center: middleLayoutCircle.inscribedHexagon.points[(cornerIndex + 1) % 6], radius: radius / 3.0 / .sqrt3)
+        var path: Path = Path()
+        path.move(to: innerLayoutCircle.inscribedHexagon.points[cornerIndex])
+        path.addLine(to: cornerHexagon.points[(cornerIndex + 2) % 6])
+        path.addLine(to: outerLayoutCircle.inscribedHexagon.points[(cornerIndex + 0) % 6])
+        path.addLine(to: cornerHexagon2.points[(cornerIndex + 5) % 6])
+        path.addLine(to: innerLayoutCircle.inscribedHexagon.points[cornerIndex])
+        path.closeSubpath()
+        return path
+    }
+    
+    private func startPointPath(innerLayoutCircle: LayoutCircle,
+                                outerLayoutCircle: LayoutCircle,
+                                cornerIndex: Int) -> Path {
+        var path: Path = Path()
+        path.move(to: outerLayoutCircle.inscribedHexagon.points[cornerIndex])
+        path.addLine(to: innerLayoutCircle.inscribedHexagon.points[cornerIndex])
+        path.addLine(to: innerLayoutCircle.inscribedHexagon.points[(cornerIndex + 5) % 6])
+        path.addLine(to: outerLayoutCircle.inscribedHexagon.points[cornerIndex])
+        path.closeSubpath()
+        return path
+    }
+    
+    private func diamondPath(innerLayoutCircle: LayoutCircle, middleLayoutCircle: LayoutCircle, outerLayoutCircle: LayoutCircle, cornerIndex: Int) -> Path {
+        var path: Path = Path()
+        path.move(to: outerLayoutCircle.inscribedHexagon.points[cornerIndex])
+        path.addLine(to: middleLayoutCircle.inscribedHexagon.points[(cornerIndex + 1) % 6])
+        path.addLine(to: innerLayoutCircle.inscribedHexagon.points[cornerIndex])
+        path.addLine(to: middleLayoutCircle.inscribedHexagon.points[(cornerIndex + 6) % 6])
+        path.addLine(to: outerLayoutCircle.inscribedHexagon.points[cornerIndex])
+        path.closeSubpath()
+        return path
+    }
+    
+    private func outerKitePath(innerLayoutCircle: LayoutCircle, outerLayoutCircle: LayoutCircle, radius: CGFloat, cornerIndex: Int) -> Path {
+        var path: Path = Path()
+        let cornerHexagon: Hexagon = Hexagon(center: outerLayoutCircle.inscribedHexagon.points[cornerIndex], radius: radius / 3.0 / .sqrt3)
+        path.move(to: cornerHexagon.center)
+        path.addLine(to: cornerHexagon.points[(cornerIndex + 2) % 6])
+        path.addLine(to: innerLayoutCircle.inscribedHexagon.points[cornerIndex % 6])
+        path.addLine(to: cornerHexagon.points[(cornerIndex + 4) % 6])
+        path.addLine(to: cornerHexagon.center)
+        path.closeSubpath()
+        return path
+    }
+    
     private func kiteLayoutPath(layoutCircle1: LayoutCircle, layoutCircle2: LayoutCircle, cornerIndex: Int) -> Path {
         var path: Path = Path()
         path.move(to: layoutCircle1.center)
